e3f6ea6b179bc918660d7c17b34da30427a9558b
mifouts <fouts.mitch15@gmail.com>
2022-09-26 17:34:20 -0400

    importing component

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,8 @@ function App() {
       <Todo></Todo>
       <Todo></Todo>
       <Todo></Todo>
-      <div>
-        <h2>Finish Frontend Simplified</h2>
-        <button>Delete</button>
       </div>
-      <div>
-        <h2>Finish the Interview Section</h2>
-        <button>Delete</button>
-      </div>
-      <div>
-        <h2>Land a 100k Job</h2>
-        <button>Delete</button>
-      </div>
-    </div>
-  );
+      )
 }
 
 export default App;
