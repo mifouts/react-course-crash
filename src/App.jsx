a2aa68b3f928d9b4bdb6d0eed611d55a26eff065
mifouts <fouts.mitch15@gmail.com>
2022-09-26 17:33:23 -0400

    using components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div>
       <h1>My Todo List</h1>
+      <Todo></Todo>
+      <Todo></Todo>
+      <Todo></Todo>
       <div>
         <h2>Finish Frontend Simplified</h2>
         <button>Delete</button>
