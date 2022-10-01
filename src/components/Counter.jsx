bb831c227cdabd2e08f2dd2e84cc620a672192e0
mifouts <fouts.mitch15@gmail.com>
2022-10-01 14:21:56 -0400

    messing with counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,23 @@
 import React, {useState} from 'react';
+
+
 function Counter() {
-    const [Counter, setCounter] = useState(0)
+    const [cart, setCart] =useState({ item: 'apple', quantity: 10 });
     const increase = () => {
-        setCounter(count => count + 1)
+        setCart(count => count + 1)
       }
       const decrease = () => {
-        setCounter(count => count - 1)
+        setCart(count => count - 1)
       }
       const reset = () => {
-        setCounter(0)
+        setCart(0)
       }
     return (
         <div className="counter">
             <span className="counter__output">{Counter}</span>
             <div className="button__container">
                 <button className="control__btn" onClick={increase}>+</button>
+                {cart.quantity}
                 <button className="control__btn" onClick={decrease}>-</button>
                 <button className="reset" onClick={reset}>Reset</button>
             </div>
