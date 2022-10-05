f469e3299da35a4d994b41766d311fa13b2e2d2f
mifouts <fouts.mitch15@gmail.com>
2022-10-05 19:58:16 -0400

    creating users route/component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,9 @@ function Nav() {
     return (
         <nav>
         <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/users/Ronaldo">Ronaldo</Link>
+        <Link to="/Messi">Messi</Link>
+        <Link to="/br4gg">David Bragg</Link>
       </nav>
     );
 }
