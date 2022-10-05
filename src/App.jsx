5b2fd8e5f1044a7fae5e4e838950e5f1d5d33303
mifouts <fouts.mitch15@gmail.com>
2022-10-05 19:53:47 -0400

    importing nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/contact.jsx'
-
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/contact.jsx';
+import Nav from "./components/Nav";
 function App() {
 
   return (
     <>
     <Router>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
