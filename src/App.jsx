7111977a522168e8c51797e0530c00fba8549683
mifouts <fouts.mitch15@gmail.com>
2022-10-05 17:50:19 -0400

    importing use effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Todo from './components/Todo.jsx'
 import Header from './components/Header.jsx';
 import Modal from './components/Modal.jsx';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
