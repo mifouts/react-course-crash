5909337b015534048348bc9fa96201a09fc6fd05
mifouts <fouts.mitch15@gmail.com>
2022-10-05 20:04:52 -0400

    making routing dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Contact from './pages/contact.jsx';
+//import About from './pages/About.jsx';
+//import Contact from './pages/contact.jsx';
 import Nav from "./components/Nav";
+import Users from "./pages/Users";
+
+
 function App() {
 
   return (
@@ -12,8 +15,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/users/:username" element={<Users />}/>
       </Routes>
     </Router>
     </>
