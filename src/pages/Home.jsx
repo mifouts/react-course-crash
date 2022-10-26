233b8f6ee3c0e01d0c6fdba1bd547fb83188aab5
mifouts <fouts.mitch15@gmail.com>
2022-10-26 19:22:57 -0400

    using axios for API

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+import axios from 'axios'
+
 function Home() {
+    useEffect(() => {
+        async function main() {
+        console.log(await axios.get("https://jsonplaceholder.typicode.com/users"))
+        }
+        main();
+    }, [])
     return <h1>Home</h1>
 }
 
