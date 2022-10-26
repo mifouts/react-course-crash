893544a9b51b63a38c8336f2ebcee8e5ac04d5f8
mifouts <fouts.mitch15@gmail.com>
2022-10-26 19:52:32 -0400

    axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 function Home() {
     const [users, setUsers] = useState ([])
     async function fetchUsers() {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
+        const { data } = await ("https://jsonplaceholder.typicode.com/users")
         setUsers(data)
         }
 
