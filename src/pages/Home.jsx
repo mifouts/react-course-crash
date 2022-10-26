8b1b0aa96e6388f243547ec6390d9158f3c428d9
mifouts <fouts.mitch15@gmail.com>
2022-10-26 19:34:07 -0400

    hitting api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react"
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
 
 function Home() {
     const [users, setUsers] = useState ([])
