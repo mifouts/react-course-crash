c38fb07befd746f62d220fa65f6b9f77e79b0750
mifouts <fouts.mitch15@gmail.com>
2022-10-26 20:39:58 -0400

    styling and making dynamic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,32 @@ import axios from "axios";
 
 
 function Home() {
-    const [users, setUsers] = useState ([])
+    const [users, setUsers] = useState([]);
     async function fetchUsers() {
-        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users")
-        setUsers(data)
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/users");
+        setUsers(data);
         }
 
     useEffect(() => {
         setTimeout(() => {
             fetchUsers();
-        }, 2000)
+        }, 500)
     }, []);
 
+    const pixels = "3px"
+
     return (
-    <div>
-        {users.length > 0
-        ? <h1>{users[0]?.name}</h1>
-        : <h1>Loading...</h1>}
+        <div>
+    {users.map((user) => {
+        return (
+    <div style= {{border: `${pixels} solid black`}}>
+       <div>{user?.id}</div>
+       <div>{user?.name}</div>
+       <div>{user?.email}</div>
+       <div>{user?.username}</div>
+    </div>
+        );
+    })}
     </div>
     );
 }
